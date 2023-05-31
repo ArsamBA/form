cbf29dce0beb923fc76f1bd9bcfba4de94432997
Arsam Bagheramiri <arsam5ba5@gmail.com>
2023-05-31 17:12:58 +0330

    necessary components added

---
+++ form/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Landing :title="title" :description="description" :buttonText="buttonText">
+      // import landing svg
+    </Landing>
+  </div>
+  <div style="border-top: 1px solid #a0a0a0; display: block; width: 100% ;"></div>
+  <div class="container">
+    // import main component
+  </div>
+</template>
+
+<script setup lang="ts">
+const title = ref("")
+const description = ref("")
+const buttonText = ref("")
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/global.scss';
+
+</style>
